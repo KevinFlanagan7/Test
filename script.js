717c3e8860bdafc8ef81b72351d0bf1041f7cea3
KevinFlanagan7 <148767900+KevinFlanagan7@users.noreply.github.com>
2024-01-15 20:03:00 +0000

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+function startQuiz() {
+    document.getElementById('report').innerHTML = `
+        <h4>Scuuuuup!!!</h4>`
+}
+
+function turnGreen() {
+    let green = document.getElementById('green');
+    green.style.backgroundColor = "green";
+}
