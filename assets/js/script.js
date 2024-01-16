0cf152bd0460655c5da2f764da65dad6dde06688
KevinFlanagan7 <148767900+KevinFlanagan7@users.noreply.github.com>
2024-01-16 18:22:01 +0000

    corrected js

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,17 +82,3 @@ const questions = [
 
 
 
-function startQuiz() {
-    document.getElementById('report').innerHTML = `
-        <h4>Scuuuuup!!!</h4>`
-}
-
-function turnGreen() {
-    let green = document.getElementById('green');
-    green.style.backgroundColor = "rgba(110, 144, 117, 0.8)";
-}
-
-function turnBack() {
-    let white = document.getElementById('green');
-    white.style.backgroundColor = "var(--white)";
-}
